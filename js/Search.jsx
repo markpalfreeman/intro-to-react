aae85d76c93ec89b173e64acdcabacfb91f65e6b
Mark Palfreeman <markpalfreeman@gmail.com>
2016-03-24 12:36:10 -0700

    Pull out ShowCard into own component & add propTypes

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,19 +1,13 @@
 const React = require('react')
 const { shows } = require('../public/data')
+const ShowCard = require('./ShowCard')
 
 const Search = () => (
   <div className='container'>
     <div className='shows'>
 
       {shows.map((show) => (
-        <div className='show-card'>
-          <img src={`public/img/posters/${show.poster}`} className='show-card-img'/>
-          <div className='show-card-text'>
-            <h3 className='show-card-title'>{show.title}</h3>
-            <h4 className='show-card-year'>({show.year})</h4>
-            <p className='show-card-description'>{show.description}</p>
-          </div>
-        </div>
+        <ShowCard show={show} key={show.imdbID}/>
       ))}
 
     </div>
