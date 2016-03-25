77ea5a10c7f7a9a33988f0cdb2c933e9b4c46cd6
Mark Palfreeman <markpalfreeman@gmail.com>
2016-03-25 12:50:15 -0700

    Build out Landing component with search functionality and form submission

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,14 +1,42 @@
 const React = require('react')
-const { Link } = require('react-router')
-
-const Landing = () => (
-  // Implicit 'return' without { }
-  <div className='home-info'>
-    <h1 className='title'>VideoViewer</h1>
-    <input className='search' type='text' placeholder='Search'/>
-    <Link to='/search' className='browse-all'> or Browse All</Link>
-  </div>
-)
+const { Link, hashHistory } = require('react-router')
 const { connector } = require('./Store')
+
+class Landing extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.handleSearchTerm = this.handleSearchTerm.bind(this)
+    this.goToForm = this.goToForm.bind(this)
+  }
+
+  handleSearchTerm (event) {
+    this.props.setSearchTerm(event.target.value)
+  }
+
+  goToForm (event) {
+    hashHistory.push('search')
+    event.preventDefault()
+  }
+
+  render () {
+    return (
+      <div className='home-info'>
+        <h1 className='title'>VideoViewer</h1>
+        <form onSubmit={this.goToSearch}>
+          <input className='search' type='text' placeholder='Search' value={this.props.searchTerm} onChange={this.handleSearchTerm}/>
+          <Link to='/search' className='browse-all'> or Browse All</Link>
+        </form>
+      </div>
+    )
+  }
+}
+
+const { func, string } = React.PropTypes
+
+Landing.propTypes = {
+  searchTerm: string,
+  setSearchTerm: func
+}
 
 module.exports = connector(Landing)
