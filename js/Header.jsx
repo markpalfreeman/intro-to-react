b50c656d8d0fc5e8fd8b8f198313fef1142476d2
Mark Palfreeman <markpalfreeman@gmail.com>
2016-03-25 11:24:26 -0700

    Add headerAction to conditionally toggle search input, pass changes via props

--- js/Header.jsx
+++ js/Header.jsx
@@ -1,19 +1,49 @@
 const React = require('react')
 const { Link } = require('react-router')
+const { func, bool, string } = React.PropTypes
 
-class Header extends React.Component {
+const Header = React.createClass({
+  propTypes: {
+    handleSearchTermChange: func,
+    showSearch: bool,
+    searchTerm: string
+  },
+
+  handleSearchTerm (event) {
+    this.props.handleSearchTermChange(event.target.value)
+  },
+
   render () {
+    let headerAction
+
+    // Conditionally show either a search bar or Back depending on 'showSearch' flag
+    if (this.props.showSearch) {
+      headerAction = (
+        <input
+          type='text'
+          className='search-input'
+          placeholder='Search'
+          value={this.props.searchTerm}
+          onChange={this.handleSearchTerm}
+        />
+      )
+    } else {
+      headerAction = (
+        <h2 className='header-back'>
+          <Link to='/search'>Back</Link>
+        </h2>
+      )
+    }
+
     return (
       <header className='header'>
         <h1 className='brand'>
           <Link to='/' className='brand-link'>VideoViewer</Link>
         </h1>
-        <h2 className='header-back'>
-          <Link to='/search'>Back</Link>
-        </h2>
+        {headerAction}
       </header>
     )
   }
-}
+})
 
 module.exports = Header
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,6 @@
 const React = require('react')
 const ShowCard = require('./ShowCard')
+const Header = require('./Header')
 const { object } = React.PropTypes
 
 const Search = React.createClass({
@@ -13,17 +14,18 @@ const Search = React.createClass({
     }
   },
 
-  handleSearchTerm (event) {
-    this.setState({searchTerm: event.target.value})
+  handleSearchTerm (searchTerm) {
+    this.setState({ searchTerm })
   },
 
   render () {
     return (
       <div className='container'>
-        <header className='header'>
-          <h1 className='brand'>VideoViewer</h1>
-          <input className='search-input' type='text' placeholder='Search' value={this.state.searchTerm} onChange={this.handleSearchTerm}/>
-        </header>
+        <Header
+          handleSearchTermChange={this.handleSearchTermChange}
+          searchTerm={this.state.searchTerm}
+          showSearch
+        />
         <div className='shows'>
           {this.props.route.shows
             .filter((show) => {
